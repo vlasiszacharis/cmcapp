0d8fa6aa9c821c37b9d81ae00bee5eb8b91e6241
vlasiszacharis <vlasisz983@gmail.com>
2023-10-19 00:38:53 +0300

    Initial Commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import Topbar from '../Topbar/Topbar'
+function Home() {
+  return (
+    <>
+        <Topbar />
+        <Navbar />
+    </>
+  )
+}
+
+export default Home
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Navbar(){
+    return(
+            <>
+                <div className="NavBar__MainWrapper">
+                    <div className="NavBar__LogoWrapper">
+
+
+                    </div>
+                    <div classNme="NavBar__LinksWrapper">
+
+
+                    </div>
+                    <div className="NavBar__SearchWrapper">
+
+
+                    </div>
+                    <div className="NavBar__MenuWrapper">
+                        
+                    </div>
+
+                </div>
+            
+            
+            </>
+    )
+}
+
+export default Navbar;
